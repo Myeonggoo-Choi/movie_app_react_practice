07e468c80fc4c6bcc5f0efa92d9d81dc31dc9128
Myeonggoo-Choi <65221299+Myeonggoo-Choi@users.noreply.github.com>
2020-08-12 10:43:10 +0200

    Updates

---
+++ precache-manifest.df5a0d9cdccc078520f75772ef5f7d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec65bb90993e0b63db5f287828696c68",
+    "url": "/movie_app_react_practice/index.html"
+  },
+  {
+    "revision": "d2b94619fbe718c02f08",
+    "url": "/movie_app_react_practice/static/css/main.51fd6d57.chunk.css"
+  },
+  {
+    "revision": "20ce81a6aa871503e993",
+    "url": "/movie_app_react_practice/static/js/2.ba314929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react_practice/static/js/2.ba314929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b94619fbe718c02f08",
+    "url": "/movie_app_react_practice/static/js/main.2e059926.chunk.js"
+  },
+  {
+    "revision": "df0610f97255ca04a9ec",
+    "url": "/movie_app_react_practice/static/js/runtime-main.ea690f97.js"
+  }
+]);
